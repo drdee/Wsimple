b44ff03a5f6ce5ab9793ea8c7dc81e3bf7da00ee
mandy gu <mgu@wealthsimple.com>
2023-03-29 22:36:06 -0400

    add RequestsPostTool

--- toolchain.py
+++ toolchain.py
@@ -2,15 +2,17 @@ from langchain.chat_models import ChatOpenAI
 from langchain.agents import load_tools, initialize_agent
 from langchain.tools import AIPluginTool
 from langchain.callbacks import get_openai_callback
+from langchain.tools.requests.tool import RequestsPostTool
+from langchain.requests import RequestsWrapper
+
 tool = AIPluginTool.from_plugin_url("http://127.0.0.1:8000/.well-known/ai-plugin.json")
 
-llm = ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo')
+llm = ChatOpenAI(temperature=0)
 tools = load_tools(["requests"] )
-tools += [tool]
+tools.extend([tool, RequestsPostTool(requests_wrapper=RequestsWrapper())])
 
 agent_chain = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
 # agent_chain.run("Retrieve my Wealthsimple Trade account status using the API on http://127.0.0.1:8000")
 
 with get_openai_callback() as cb:
-	agent_chain.run("Fund my Wealthsimple Trade account with $1 using the API on http://127.0.0.1:8000 and use as action the WSTrade plugin")
-	#agent_chain.run("What stock has the highest return in the last 30 days using Wealthsimple Trade API")
+    agent_chain.run("Make a deposit of 1 dollar to my account on http://127.0.0.1:8000/make_deposit from the WSTrade plugin")
